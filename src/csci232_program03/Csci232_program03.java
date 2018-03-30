be7cf06cc0ec56f065dfad84433bebfcfedaaa1e
dallas-legrande <33949897+dallas-legrande@users.noreply.github.com>
2018-03-29 22:17:22 -0600

    Update Csci232_program03.java
    reads in a .csv file and sends it to the dataArray class to build a matrix. The infinity symbol converted to ?'s when saved as a .csv file. The ?'s are converted to Integer.MAX_VALUE.

--- src/csci232_program03/Csci232_program03.java
+++ src/csci232_program03/Csci232_program03.java
@@ -1,7 +1,7 @@
 package csci232_program03;
 
 /**
- * Authors: Selene Smith
+ * Authors: Selene Smith, Dallas LeGrande
  * Date: 3/29/18
  * 
  * Overview:
@@ -24,10 +24,72 @@ package csci232_program03;
  *          >> start with initial matrix
  */
 
-public class Csci232_program03 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+
+public class csci232_program03 {
+    
+    private int counter = 0; //variable to count how big the array needs to be
+    private dataArray array; //variable to hold the array that is the table
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        csci232_program03 go = new csci232_program03();
+        go.readWord();
     }
+    //reads the file line by line then separates it into word by word
+    public void readWord()
+    {
+        int line1 = 1; //flag for when the first line of the file is being read in
+        int num; //int for converting strings to ints
+        
+        try{ 
+            //provides a path to the file being read from
+            Path path =  Paths.get("./input/input.csv");
+            
+            //opens the bufferedreader to be able to read a file. The file location is held in the variable "path"
+            try (BufferedReader reader = Files.newBufferedReader(path)){
+               String line;
+               
+               //reads line by line until the line is null and the document is complete
+               while ((line = reader.readLine()) != null)
+               {
+                   //splits the line every time there is a comma and puts each word into an array
+                   String[] words = line.split(",");
+                   if(line1 == 1) //if it's the first line read in then it has the table labels
+                   {
+                       counter = words.length; //count the number of elements in line 1
+                       array = new dataArray(counter); //creates a 2D array with the size of the elements in line 1
+                       line1++; //increases the line count so that integers can be read in
+                       for (String word : words) { //puts all the labels into the array
+                       num = (int)word.charAt(0); //converts the labels to ascii numbers
+                       array.fillMatrix(num); //inserts the labels into the first row of the array                     
+                       }//end of for each loop
+                   }//end of if statement
+                   else{ //if it's not the first line then convert the string to an int
+                        for (String word : words) {
+                            if(word.equals("?")) //if the string is a ? then it is infinity. Which is converted to the integer max value
+                            {
+                                num = Integer.MAX_VALUE;   
+                            }
+                            else{ //otherwise convert the string to an int
+                                num = Integer.parseInt(word);
+                            }
+                        array.fillMatrix(num); //inserts the number into the array                    
+                    }//end of for each loop
+                   }//end of else
+               }//end of while loop
+               array.print(); //print the array to see the table
+           }//end of try for buffered reader
+        }//end of try for paths
+        catch (IOException e){
+            System.out.println("File not found");
+              }
+    }//end of readWord method
+}//end of class
+
     
-}
+
